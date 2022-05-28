022b455e9051aa54abf1daaed229fc96f987a1fe
gonza1020 <gonzalozoloaga44@gmail.com>
2022-05-28 12:36:09 -0300

    Refactor name app

--- src/main/java/com/disney/ApiApplication.java
+++ src/main/java/com/disney/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.disney.api;
+package com.disney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/disney/ApiApplicationTests.java
+++ src/test/java/com/disney/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.disney.api;
+package com.disney;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
