b548a9ec4b05609e27c8e2fbc99fd0f0b39fff16
gonza1020 <gonzalozoloaga44@gmail.com>
2022-05-29 22:23:18 -0300

    Config files

--- src/main/java/com/disney/ApiApplication.java
+++ src/main/java/com/disney/ApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@SpringBootApplication()
 public class ApiApplication {
 
     public static void main(String[] args) {
