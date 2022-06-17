b502444f92ca206bf76100cc17434c86491a4bcf
gonza1020 <gonzalozoloaga44@gmail.com>
2022-06-17 13:04:29 -0300

    DA-1 Endpoints added

--- src/main/java/com/disney/service/FilmServiceImpl.java
+++ src/main/java/com/disney/service/FilmServiceImpl.java
@@ -1,4 +1,5 @@
 package com.disney.service;
+import com.disney.model.Character;
 import com.disney.model.Film;
 import com.disney.repository.FilmRepository;
 import com.disney.repository.GenreRepository;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FilmServiceImpl implements BaseService<Film> {
@@ -31,24 +33,56 @@ public class FilmServiceImpl implements BaseService<Film> {
 
     @Override
     public Film save(Film film) {
-        return movieRepository.save(film);
+        if(checkNull(film)){
+            return movieRepository.save(film);
+        }
+        return null;
     }
 
     @Override
     public Film findById(Long id) {
-        Film film = movieRepository.findById(id).get();
-
-        return null;
+        return  movieRepository.findById(id).get();
     }
 
     @Override
     public void delete(Long id) {
 
+        Optional<Film> film = movieRepository.findById(id);
+
+        if(film.isPresent()){
+            for(Character c: film.get().getCharacters()){
+                c.getFilms().remove(film.get());
+            }
+            movieRepository.delete(film.get());
+        }
     }
 
     @Override
     public Film update(Film entity, Long id) {
+
+        Optional<Film> film = movieRepository.findById(id);
+
+        if(film.isPresent()){
+            film.get().setTitle(entity.getTitle());
+            film.get().setReleaseDate(entity.getReleaseDate());
+            film.get().setImage(entity.getImage());
+            film.get().setGenre(entity.getGenre());
+            film.get().setCalification((entity.getCalification()));
+            if(checkNull(film.get())){
+                return null;
+            }
+            return movieRepository.save(film.get());
+        }
         return null;
+    }
+
+    public boolean checkNull(Film film){
+        if(film.getCalification() == 0) return true;
+        if(film.getGenre() == null) return true;
+        if(film.getImage() == null) return true;
+        if (film.getReleaseDate() == null) return true;
+        if(film.getTitle() == null) return true;
+        return (film.getType() == null);
     }
 
     @Override
